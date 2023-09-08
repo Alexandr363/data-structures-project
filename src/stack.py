a230b6671df50fddd16837c30c107190ba77dcde
Alexandr Lebedev <alexandr.lebedev.sp@gmail.com>
2023-09-08 14:46:21 +0300

    init

--- src/stack.py
+++ src/stack.py
@@ -1,34 +1,32 @@
 class Node:
-    """Класс для узла стека"""
-
+    """
+    Класс для узла стека
+    """
     def __init__(self, data, next_node):
         """
         Конструктор класса Node
-
-        :param data: данные, которые будут храниться в узле
         """
-        pass
+        self.data = data
+        self.next_node = next_node
 
 
 class Stack:
-    """Класс для стека"""
-
+    """
+    Класс для стека
+    """
     def __init__(self):
-        """Конструктор класса Stack"""
-        pass
+        """
+        Конструктор класса Stack
+        """
+        self.stack = []
+        self.top = None
 
-    def push(self, data):
+    def push(self, data) -> None:
         """
         Метод для добавления элемента на вершину стека
-
-        :param data: данные, которые будут добавлены на вершину стека
         """
-        pass
+        next_node = self.top
+        new_top = Node(data, next_node)
+        self.top = new_top
 
-    def pop(self):
-        """
-        Метод для удаления элемента с вершины стека и его возвращения
-
-        :return: данные удаленного элемента
-        """
-        pass
+        self.stack.append(data)
--- tests/test_stack.py
+++ tests/test_stack.py
@@ -1,1 +1,10 @@
 """Здесь надо написать тесты с использованием unittest для модуля stack."""
+import unittest
+from src.stack import Stack
+
+test = Stack()
+
+
+class TestStack(unittest.TestCase):
+    def test_push(self):
+        self.assertEqual(test.push(10), test.stack[0])
